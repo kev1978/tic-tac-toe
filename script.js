2a627359d774fad7db2ee59f46e367ebcee18523
Kevin Cooksley <kevincooksley63@gmail.com>
2022-08-18 09:07:18 +0000

    add js player X and O

---
+++ script.js
@@ -0,0 +1,7 @@
+const tiles = document.querySelectorAll(".tile");
+const PLAYER_X = "X";
+const PLAYER_O = "O";
+let turn = PLAYER_X;
+
+const boardState = Array(tiles.length);
+boardState.fill(null);
