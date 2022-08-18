3de271fe5e0670be54f7deb34ff6c3524dfb7979
Kevin Cooksley <kevincooksley63@gmail.com>
2022-08-18 09:42:21 +0000

    having trouble getting my click to work so it displays an X or Oon grid need to trouble shoot it

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ const strike = document.getElementById("strike");
 const gameOverArea = document.getElementById("game-over-area");
 const gameOverText = document.getElementById("game-over-text");
 const playAgain = document.getElementById("play-again");
+const gameOverSound = new Audio("sounds/game_over.wav");
+const click = new Audio("sounds/click.wav");
+
+tiles.forEach(tile=>tile.addEventListener("click", tileClick));
+function tileClick(event) {
+    if (gameOverArea.classList.contains("visible")) {
+        return;
+    }
+
+    const tile = event.target;
+    const tileNumber = tile.dataset.index;
+    if (tile.innerText !="") {
+        return;
+
+        if (turn === PLAYER_X) {
+            tile.innerText = PLAYER_X;
+            boardState[tileNumber - 1] = PLAYER_X;
+            turn = PLAYER_O;
+        }
+    }
+}
